faec5a124b3a7f0a35535c4a9c86e7940ae46a3c
Vlad Ulici <149094040+vulici@users.noreply.github.com>
2023-12-13 20:33:02 -0500

    Add files via upload

---
+++ String-1/without_end.py
@@ -0,0 +1,11 @@
+#Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+#without_end('Hello') → 'ell'
+#without_end('java') → 'av'
+#without_end('coding') → 'odin'
+
+def without_end(str):
+  if len(str) < 2:
+    return str
+  else:
+    return str[1:-1]
